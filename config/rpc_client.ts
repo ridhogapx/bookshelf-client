6485bd63119a16d31d687cbfc851c27b4c9bfdcf
RageNeko26 <rneko2006@gmail.com>
2023-07-12 23:35:34 +0700

    feat: Add config RPC Client

---
+++ config/rpc_client.ts
@@ -0,0 +1,29 @@
+// Path module
+const path = require("path")
+
+// GRPC Client
+const grpc = require("@grpc/grpc-js")
+
+// Proto loader
+const protoLoader = require("@grpc/proto-loader")
+
+// Proto Path
+const PROTO_PATH = path.resolve(__dirname, "../../proto/owner.proto")
+
+// Options RPC
+const options = {
+    keepCase: true,
+    longs: String,
+    enums: String,
+    defaults: true,
+    oneofs: true,
+  }
+
+const packageDefinition = protoLoader.loadSync(PROTO_PATH, options)
+const proto = grpc.loadPackageDefinition(packageDefinition).proto
+
+// Client RPC
+const client = new proto.OwnerService(
+    "localhost:50051",
+    grpc.credentials.createInsecure()
+)
