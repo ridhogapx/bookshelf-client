eee2b25b0051261ce943de85217cdd1241758a1f
RageNeko26 <rneko2006@gmail.com>
2023-07-13 15:01:33 +0700

    feat: Adding handler route for listing all Book Owners

--- src/controller/owner_controller.ts
+++ src/controller/owner_controller.ts
@@ -1,5 +1,10 @@
 import { Request, Response } from "express"
+import client from "../../config/rpc_client"
+import { Owners } from "../../types/Owner"
 
 export const GetOwner = (req: Request, res: Response): void => {
     // Handler
+    client.GetOwners({}, (err: string, resRPC: Owners) => {
+        res.json(resRPC)
+    })
 }
